2ef48d0d765504a893c264bb23c9796fc5e3ffff
AbdelwahabTop <mramged936@gmail.com>
2023-04-08 19:28:02 +0200

    light bulb

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import profilePic from "../../public/images/profile/pngwing.com (6).png";
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 
 export default function Home() {
   return (
@@ -17,14 +18,14 @@ export default function Home() {
       <main className="flex items-center text-dark w-full min-h-screen">
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
-            <div className="w-1/1">
+            <div className="w-1/1 ml-16">
               <Image
                 src={profilePic}
                 alt="Abdelwahab"
                 className="w-full h-auto"
               />
             </div>
-            <div className="w-[700px] flex flex-col items-center self-center">
+            <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
                 className="!text-6xl !text-left"
@@ -59,6 +60,10 @@ export default function Home() {
         </Layout>
 
         <HireMe />
+
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image src={lightBulb} alt="Abdo" className="w-full h-auto" />
+        </div>
       </main>
     </>
   );
