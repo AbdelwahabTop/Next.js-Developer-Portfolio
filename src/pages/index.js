910393231ecc6567c67dac216c9b9ba6355b4f88
AbdelwahabTop <mramged936@gmail.com>
2023-04-08 16:42:22 +0200

    resume and contact links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import AnimatedText from "@/components/AnimatedText";
+import { LinkArrow } from "@/components/icons";
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import profilePic from "../../public/images/profile/pngwing.com (6).png";
 
 export default function Home() {
@@ -22,13 +24,35 @@ export default function Home() {
               />
             </div>
             <div className="w-[700px] flex flex-col items-center self-center">
-              <AnimatedText text="Turning Vision Into Reality With Code And Design." className="!text-6xl !text-left"/>
-              <p>
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="!text-6xl !text-left"
+              />
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/dummy.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold 
+                  hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
+                  "
+                  download={true}
+                >
+                  Resume <LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link
+                  href="mailto:mramged936@gmail.com"
+                  target={"_blank"}
+                  className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
