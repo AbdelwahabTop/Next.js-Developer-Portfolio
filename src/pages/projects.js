27330c85fb20deb3dd3a04423f68fdc75d4edac5
AbdelwahabTop <mramged936@gmail.com>
2023-04-09 15:48:47 +0200

    Project Page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,7 +9,9 @@ import project1 from "../../public/images/projects/crypto-screener-cover-image.j
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+    <article className="w-full flex items-center justify-between relative rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
+
       <Link
         href={link}
         target="_blank"
@@ -48,6 +50,8 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 const Project = ({ type, title, img, link, github }) => {
   return (
     <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
+
       <Link
         href={link}
         target="_blank"
@@ -69,7 +73,7 @@ const Project = ({ type, title, img, link, github }) => {
           <Link
             href={link}
             target="_blank"
-            className="ml-4 text-lg font-semibold"
+            className="text-lg font-semibold underline"
           >
             Visit
           </Link>
@@ -96,7 +100,7 @@ const projects = () => {
             className="mb-16"
           />
 
-          <div className="grid grid-cols-12 gap-24">
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 title="Crypto Screener Application"
@@ -118,9 +122,45 @@ const projects = () => {
                 github="/"
               />
             </div>
-            <div className="col-span-6">Project-2</div>
-            <div className="col-span-6">Project-3</div>
-            <div className="col-span-6">Project-4</div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                link="/"
+                type="Featured Project"
+                img={project1}
+                github="/"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Crypto Screener Application"
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                type="Featured Project"
+                img={project1}
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                link="/"
+                type="Featured Project"
+                img={project1}
+                github="/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                link="/"
+                type="Featured Project"
+                img={project1}
+                github="/"
+              />
+            </div>
           </div>
         </Layout>
       </main>
