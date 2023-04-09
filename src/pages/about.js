0f38fdd5844eb5afdc0c45bdf96a787d3c1a9558
AbdelwahabTop <mramged936@gmail.com>
2023-04-09 07:59:49 +0200

    About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,34 @@
 import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import profilePic from "../../public/images/profile/abdelwahab-crop.jpeg";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
 
 const about = () => {
   return (
@@ -16,6 +41,7 @@ const about = () => {
         <Layout className="pt-16">
           <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
           <div className="grid w-full grid-cols-8 gap-16">
+            {/* Paragraph */}
             <div className="col-span-3 flex flex-col items-start justify-start">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
                 Biography
@@ -40,7 +66,7 @@ const about = () => {
                 your next project.
               </p>
             </div>
-
+            {/* Image */}
             <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
               <Image
@@ -48,6 +74,35 @@ const about = () => {
                 alt="Abdo"
                 className="w-full h-auto rounded-2xl"
               />
+            </div>
+            {/* counter */}
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={10} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  satisfied clients
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={15} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  projects completed
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={1} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  years of experience
+                </h2>
+              </div>
             </div>
           </div>
         </Layout>
