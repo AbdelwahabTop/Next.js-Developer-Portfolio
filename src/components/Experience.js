c19c11c141dc932c63ebe046c0dca054ce3165c6
AbdelwahabTop <mramged936@gmail.com>
2023-04-10 11:08:44 +0200

    Experience Dark Mode

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -28,12 +28,12 @@ const Details = ({
           <a
             href={companyLink}
             target="_blank"
-            className={`text-primary capitalize ${size}`}
+            className={`text-primary dark:text-primaryDark capitalize ${size}`}
           >
             @{company}
           </a>
         </h3>
-        <span className="capitalize font-medium text-dark/75">
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
           {time} | {address}
         </span>
         <p className="font-medium w-full">{work}</p>
@@ -58,7 +58,7 @@ const Experience = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
         />
 
         <ul className="w-full flex flex-col items-start justify-between ml-4">
