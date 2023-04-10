8099f6d2fdadc5ef3bb745d3867a32d4aee0b334
AbdelwahabTop <mramged936@gmail.com>
2023-04-10 08:03:53 +0200

    dark mood

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -30,9 +30,9 @@ const singelWord = {
 
 const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className="w-full mx-auto flex items-center justify-center text-center overflow-hidden">
+    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
       <motion.h1
-        className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+        className={`inline-block w-full text-dark font-bold capitalize text-8xl dark:text-light ${className}`}
         variants={quote}
         initial="initial"
         animate="animate"
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,7 +27,7 @@ const CustomLink = ({ href, title, className = "" }) => {
         className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5
         group-hover:w-full transition-[width] ease duration-300 
         ${router.asPath === href ? "w-full" : "w-0"}
-        `}
+        dark:bg-light`}
       >
         &nbsp;
       </span>
@@ -39,7 +39,7 @@ const NavBar = () => {
   const [mode, setMode] = useThemeSwitcher();
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -96,7 +96,9 @@ const NavBar = () => {
 
         <button
           onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className="ml-3 flex items-center justify-center rounded-full"
+          className={`ml-3 flex items-center justify-center rounded-full ${
+            mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+          }`}
         >
           {mode === "dark" ? (
             <SunIcon className={"fill-dark"} />
