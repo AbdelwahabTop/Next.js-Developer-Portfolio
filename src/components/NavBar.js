4efd8a6bc7e8a3faaa4df669b21b6bfa7854cb9f
AbdelwahabTop <mramged936@gmail.com>
2023-04-11 11:05:35 +0200

    nav respo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import {
   TwitterIcon,
   DribbbleIcon,
@@ -37,76 +37,104 @@ const CustomLink = ({ href, title, className = "" }) => {
 
 const NavBar = () => {
   const [mode, setMode] = useThemeSwitcher();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
-      <nav>
-        <CustomLink href="/" title="Home" className="mr-6" />
-        <CustomLink href="/about" title="About" className="mx-6" />
-        <CustomLink href="/projects" title="Projects" className="mx-6" />
-        {/* <CustomLink href="/articles" title="Articles" className="ml-4" /> */}
-      </nav>
+      <button
+        className="flex-col justify-center items-center hidden lg:flex"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          }`}
+        ></span>
+      </button>
 
-      <nav className="flex items-center justify-center flex-wrap">
-        <motion.a
-          href="https://twitter.com/abdelwa7a_amgad"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <TwitterIcon />
-        </motion.a>
-        <motion.a
-          href="https://github.com/AbdelwahabTop"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <GithubIcon />
-        </motion.a>
-        <motion.a
-          href="https://www.linkedin.com/in/abdelwahab-amgad/"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <LinkedInIcon />
-        </motion.a>
-        <motion.a
-          href="https://www.instagram.com/abdoabdel918/"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <Instagram />
-        </motion.a>
-        <motion.a
-          href="https://www.youtube.com/@muscularsaitama3473"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 ml-3"
-        >
-          <Youtub />
-        </motion.a>
+      <div className="w-full flex justify-between items-center lg:hidden">
+        <nav>
+          <CustomLink href="/" title="Home" className="mr-6" />
+          <CustomLink href="/about" title="About" className="mx-6" />
+          <CustomLink href="/projects" title="Projects" className="mx-6" />
+          {/* <CustomLink href="/articles" title="Articles" className="ml-4" /> */}
+        </nav>
 
-        <button
-          onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className={`ml-3 flex items-center justify-center rounded-full p-1 
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://twitter.com/abdelwa7a_amgad"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <TwitterIcon />
+          </motion.a>
+          <motion.a
+            href="https://github.com/AbdelwahabTop"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <GithubIcon />
+          </motion.a>
+          <motion.a
+            href="https://www.linkedin.com/in/abdelwahab-amgad/"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <LinkedInIcon />
+          </motion.a>
+          <motion.a
+            href="https://www.instagram.com/abdoabdel918/"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <Instagram />
+          </motion.a>
+          <motion.a
+            href="https://www.youtube.com/@muscularsaitama3473"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 ml-3"
+          >
+            <Youtub />
+          </motion.a>
+
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-3 flex items-center justify-center rounded-full p-1 
           ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
           `}
-        >
-          {mode === "dark" ? (
-            <MoonIcon className={"fill-dark"} />
-          ) : (
-            <SunIcon className={"fill-dark"} />
-          )}
-        </button>
-      </nav>
+          >
+            {mode === "dark" ? (
+              <MoonIcon className={"fill-dark"} />
+            ) : (
+              <SunIcon className={"fill-dark"} />
+            )}
+          </button>
+        </nav>
+      </div>
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
