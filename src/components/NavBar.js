512bb5f203d916cb1248e463d22436bad980e831
AbdelwahabTop <mramged936@gmail.com>
2023-04-11 11:59:48 +0200

    Mobile Menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,6 +35,34 @@ const CustomLink = ({ href, title, className = "" }) => {
   );
 };
 
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+
+  return (
+    <button
+      href={href}
+      className={`${className} relative group text-light dark:text-dark my-2`}
+      onClick={handleClick}
+    >
+      {title}
+
+      <span
+        className={`h-[1px] inline-block bg-light absolute left-0 -bottom-0.5
+        group-hover:w-full transition-[width] ease duration-300 
+        ${router.asPath === href ? "w-full" : "w-0"}
+        dark:bg-dark`}
+      >
+        &nbsp;
+      </span>
+    </button>
+  );
+};
+
 const NavBar = () => {
   const [mode, setMode] = useThemeSwitcher();
   const [isOpen, setIsOpen] = useState(false);
@@ -44,7 +72,8 @@ const NavBar = () => {
   };
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative">
+      {/* Toggle Icon */}
       <button
         className="flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}
@@ -66,6 +95,7 @@ const NavBar = () => {
         ></span>
       </button>
 
+      {/* navbar */}
       <div className="w-full flex justify-between items-center lg:hidden">
         <nav>
           <CustomLink href="/" title="Home" className="mr-6" />
@@ -135,6 +165,101 @@ const NavBar = () => {
           </button>
         </nav>
       </div>
+
+      {/* Mobile Menu */}
+      {isOpen ? (
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed 
+            top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 
+            rounded-lg backdrop-blur-md py-32
+          "
+        >
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink
+              href="/"
+              title="Home"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/about"
+              title="About"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/projects"
+              title="Projects"
+              className=""
+              toggle={handleClick}
+            />
+            {/* <CustomLink href="/articles" title="Articles" className="ml-4" /> */}
+          </nav>
+
+          <nav className="flex items-center justify-center flex-wrap mt-2">
+            <motion.a
+              href="https://twitter.com/abdelwa7a_amgad"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mr-3 sm:mx-1"
+            >
+              <TwitterIcon />
+            </motion.a>
+            <motion.a
+              href="https://github.com/AbdelwahabTop"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1"
+            >
+              <GithubIcon />
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/abdelwahab-amgad/"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <LinkedInIcon />
+            </motion.a>
+            <motion.a
+              href="https://www.instagram.com/abdoabdel918/"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 dark:bg-dark rounded-full sm:mx-1"
+            >
+              <Instagram />
+            </motion.a>
+            <motion.a
+              href="https://www.youtube.com/@muscularsaitama3473"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 ml-3 sm:mx-1"
+            >
+              <Youtub />
+            </motion.a>
+
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`ml-3 flex items-center justify-center rounded-full p-1 
+          ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+          `}
+            >
+              {mode === "dark" ? (
+                <MoonIcon className={"fill-dark"} />
+              ) : (
+                <SunIcon className={"fill-dark"} />
+              )}
+            </button>
+          </nav>
+        </motion.div>
+      ) : null}
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
