a2ee13f7c56e41440b926b70f7bfe62d4bca160e
AbdelwahabTop <mramged936@gmail.com>
2023-04-12 13:02:26 +0200

    About page responsive

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -58,8 +58,7 @@ const Experience = () => {
       <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light 
-          md:w-[2px] md:left-[30px] xs:left-[20px]"
+          className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark  origin-top  dark:bg-primaryDark dark:shadow-3xl"
         />
 
         <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
